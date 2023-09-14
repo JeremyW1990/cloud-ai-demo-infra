5b4000504d6b8d371331bea3e4a4b246516c10b7
Jeremy Wang <shijie.wang1990@gmail.com>
2023-09-14 10:44:56 -0500

    fix some bugs

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -3,8 +3,8 @@
 module "eks_cluster" {
   source = "./modules/eks"
 
-  vpc_id          = aws_vpc.cloud-ai-vpc.vpc_id
-  private_subnets = aws_vpc.cloud-ai-vpc.private_subnets
+  vpc_id          = aws_vpc.cloud-ai-vpc.id
+  private_subnets = aws_vpc.cloud-ai-vpc.subnet_ids
   cluster_version = "1.27"
   
   cluster_name = "my-cluster"
