c0891691014e86a3ba3e5d38828bac023a3d9e05
Jeremy Wang <shijie.wang1990@gmail.com>
2023-09-15 13:46:05 -0500

    wip

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -13,24 +13,24 @@ resource "aws_eks_cluster" "this" {
   ]
 }
 
-resource "aws_eks_node_group" "this" {
-  cluster_name    = aws_eks_cluster.this.name
-  node_group_name = "cloud-ai-nodes"
-  node_role_arn   = aws_iam_role.eks_nodes.arn
-  subnet_ids = var.private_subnets
+# resource "aws_eks_node_group" "this" {
+#   cluster_name    = aws_eks_cluster.this.name
+#   node_group_name = "cloud-ai-nodes"
+#   node_role_arn   = aws_iam_role.eks_nodes.arn
+#   subnet_ids = var.private_subnets
 
-  instance_types = ["t3.micro"]
+#   instance_types = ["t3.micro"]
 
-  scaling_config {
-    desired_size = 3
-    min_size     = 2
-    max_size     = 3
-  }
+#   scaling_config {
+#     desired_size = 3
+#     min_size     = 2
+#     max_size     = 3
+#   }
 
-  depends_on = [ 
-    kubernetes_config_map.aws_auth,
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-  ]
-}
+#   depends_on = [ 
+#     kubernetes_config_map.aws_auth,
+#     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+#     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+#     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+#   ]
+# }
