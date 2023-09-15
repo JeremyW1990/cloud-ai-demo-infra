fe052905c073043cf8ee456818a75dc569f5abd3
Jeremy Wang <shijie.wang1990@gmail.com>
2023-09-15 17:40:34 -0500

    add NAT, IGW and route tables

--- vpc.tf
+++ vpc.tf
@@ -55,37 +55,68 @@ resource "aws_security_group" "cloud-ai-allow-all" {
   }
 }
 
-resource "aws_eip" "nat_gateway_eip" {
+# Internet Gateway
+resource "aws_internet_gateway" "cloud-ai-igw" {
+  vpc_id = aws_vpc.cloud-ai-vpc.id
+
+  tags = {
+    Name = "Cloud-AI Internet Gateway"
+  }
+}
+
+# Public subnet for NAT Gateway
+resource "aws_subnet" "cloud-ai-public-subnet-us-east-1a" {
+  vpc_id            = aws_vpc.cloud-ai-vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Cloud-AI Public Subnet us-east-1a"
+  }
+}
+
+# Create an Elastic IP for NAT Gateway
+resource "aws_eip" "nat_eip" {
   vpc = true
-
-  tags = {
-    Name = "NAT Gateway EIP"
-  }
 }
 
+# NAT Gateway
 resource "aws_nat_gateway" "cloud-ai-nat-gateway" {
-  allocation_id = aws_eip.nat_gateway_eip.id
-  subnet_id     = aws_subnet.public_subnet.id
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.cloud-ai-public-subnet-us-east-1a.id
 
   tags = {
-     Name = "Cloud-AI NAT Gateway"
+    Name = "Cloud-AI NAT Gateway"
   }
 }
 
-# Associate route table with subnet a
-resource "aws_route_table_association" "private_1a" {
+# Route table for private subnets to route through NAT Gateway
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.cloud-ai-vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.cloud-ai-nat-gateway.id
+  }
+
+  tags = {
+    Name = "Private Subnet Route Table"
+  }
+}
+
+# Associate private subnets with the new route table
+resource "aws_route_table_association" "private-subnet-association-1a" {
   subnet_id      = aws_subnet.cloud-ai-private-subnet-us-east-1a.id
-  route_table_id = aws_route_table.private.id
+  route_table_id = aws_route_table.private-route-table.id
 }
 
-# Associate route table with subnet b
-resource "aws_route_table_association" "private_1b" {
-  subnet_id      = aws_subnet.cloud-ai-private-subnet-us-east-1b.id 
-  route_table_id = aws_route_table.private.id
+resource "aws_route_table_association" "private-subnet-association-1b" {
+  subnet_id      = aws_subnet.cloud-ai-private-subnet-us-east-1b.id
+  route_table_id = aws_route_table.private-route-table.id
 }
 
-# Associate route table with subnet c  
-resource "aws_route_table_association" "private_1c" {
+resource "aws_route_table_association" "private-subnet-association-1c" {
   subnet_id      = aws_subnet.cloud-ai-private-subnet-us-east-1c.id
-  route_table_id = aws_route_table.private.id 
+  route_table_id = aws_route_table.private-route-table.id
 }
+
