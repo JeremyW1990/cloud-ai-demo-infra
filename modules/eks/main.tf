39f211f94cf2069386e371558a75e20cde00aad6
Jeremy Wang <shijie.wang1990@gmail.com>
2023-09-15 12:42:06 -0500

    add nodegroup back

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -13,21 +13,23 @@ resource "aws_eks_cluster" "this" {
   ]
 }
 
-# resource "aws_eks_node_group" "this" {
-#   cluster_name    = aws_eks_cluster.this.name
-#   node_group_name = "cloud-ai-nodes"
-#   node_role_arn   = aws_iam_role.eks_nodes.arn
-#   subnet_ids = var.private_subnets
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "cloud-ai-nodes"
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+  subnet_ids = var.private_subnets
 
-#   scaling_config {
-#     desired_size = 3
-#     min_size     = 2
-#     max_size     = 3
-#   }
+  instance_types = ["t3.micro"]
+  
+  scaling_config {
+    desired_size = 3
+    min_size     = 2
+    max_size     = 3
+  }
 
-#   depends_on = [ 
-#     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-#     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-#     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-#   ]
-# }
+  depends_on = [ 
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
