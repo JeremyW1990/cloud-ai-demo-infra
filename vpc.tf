9ac2bb07798d6c5f6cd9a2e75fab2853e5d501eb
Jeremy Wang <shijie.wang1990@gmail.com>
2023-09-15 17:01:32 -0500

    add a NAT to subnets

--- vpc.tf
+++ vpc.tf
@@ -54,3 +54,47 @@ resource "aws_security_group" "cloud-ai-allow-all" {
     Name = "Cloud-AI Allow All"
   }
 }
+
+resource "aws_eip" "nat_gateway_eip" {
+  vpc = true
+
+  tags = {
+    Name = "NAT Gateway EIP"
+  }
+}
+
+resource "aws_nat_gateway" "cloud-ai-nat-gateway" {
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+     Name = "Cloud-AI NAT Gateway"
+  }
+}
+
+resource "aws_nat_gateway" "cloud-ai-nat-gateway" {
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+     Name = "Cloud-AI NAT Gateway"
+  }
+}
+
+# Associate route table with subnet a
+resource "aws_route_table_association" "private_1a" {
+  subnet_id      = aws_subnet.cloud-ai-private-subnet-us-east-1a.id
+  route_table_id = aws_route_table.private.id
+}
+
+# Associate route table with subnet b
+resource "aws_route_table_association" "private_1b" {
+  subnet_id      = aws_subnet.cloud-ai-private-subnet-us-east-1b.id 
+  route_table_id = aws_route_table.private.id
+}
+
+# Associate route table with subnet c  
+resource "aws_route_table_association" "private_1c" {
+  subnet_id      = aws_subnet.cloud-ai-private-subnet-us-east-1c.id
+  route_table_id = aws_route_table.private.id 
+}
