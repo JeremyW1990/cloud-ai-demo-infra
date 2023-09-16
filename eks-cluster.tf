2020ccc7270443754e577f067d8b0ff31625e4d2
Jeremy Wang <shijie.wang1990@gmail.com>
2023-09-15 21:44:36 -0500

    wip

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,3 +1,17 @@
+data "aws_eks_cluster" "cluster" {
+  name = modules.eks_cluster.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks-cluster.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
+
 module "eks_cluster" {
   source = "./modules/eks"
 
@@ -7,3 +21,4 @@ module "eks_cluster" {
   
   cluster_name = "cloud-ai-demo"
 }
+
--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.0" # adjust the version as per your needs
+      version = "~> 3.0" # adjust the version as per your needs
     }
   }
 
@@ -12,3 +12,4 @@ terraform {
     region = "us-east-1"
   }
 }
+
