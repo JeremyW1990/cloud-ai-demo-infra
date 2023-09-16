a33c37b989b93b9b216b1c3ec819d198a3c7875e
Jeremy Wang <shijie.wang1990@gmail.com>
2023-09-16 15:24:20 -0500

    commnet out data

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,16 +1,16 @@
-data "aws_eks_cluster" "cluster" {
-  name = "cloud-ai-demo"
-}
+# data "aws_eks_cluster" "cluster" {
+#   name = "cloud-ai-demo"
+# }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = "cloud-ai-demo"
-}
+# data "aws_eks_cluster_auth" "cluster" {
+#   name = "cloud-ai-demo"
+# }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-}
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+# }
 
 module "eks_cluster" {
   source = "./modules/eks"
