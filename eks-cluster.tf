b1d937edc5231eb06c2d7efb31d3fd5a4e7d1eda
Jeremy Wang <shijie.wang1990@gmail.com>
2023-09-16 15:29:52 -0500

    comment out eks tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -12,13 +12,13 @@
 #   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 # }
 
-module "eks_cluster" {
-  source = "./modules/eks"
+# module "eks_cluster" {
+#   source = "./modules/eks"
 
-  vpc_id          = aws_vpc.cloud-ai-vpc.id
-  subnets = [aws_subnet.cloud-ai-public-subnet-us-east-1a.id, aws_subnet.cloud-ai-public-subnet-us-east-1b.id, aws_subnet.cloud-ai-public-subnet-us-east-1c.id]
-  cluster_version = "1.27"
+#   vpc_id          = aws_vpc.cloud-ai-vpc.id
+#   subnets = [aws_subnet.cloud-ai-public-subnet-us-east-1a.id, aws_subnet.cloud-ai-public-subnet-us-east-1b.id, aws_subnet.cloud-ai-public-subnet-us-east-1c.id]
+#   cluster_version = "1.27"
   
-  cluster_name = "cloud-ai-demo"
-}
+#   cluster_name = "cloud-ai-demo"
+# }
 
