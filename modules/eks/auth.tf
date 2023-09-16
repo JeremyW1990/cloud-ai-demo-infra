07109cb6c77c47a437fcb447faf229426a75e504
Jeremy Wang <shijie.wang1990@gmail.com>
2023-09-15 21:34:28 -0500

    change provider

--- modules/eks/auth.tf
+++ modules/eks/auth.tf
@@ -1,15 +1,18 @@
+data "aws_eks_cluster" "this" {
+  name = aws_eks_cluster.this.name
+}
+
 data "aws_eks_cluster_auth" "this" {
   name = aws_eks_cluster.this.name
 }
 
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
 locals {
   aws_auth_config = yamldecode(file("${path.module}/aws-auth-configmap.yaml"))
-}
-
-provider "kubernetes" {
-  host                   = aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
 }
 
 resource "kubernetes_config_map" "aws_auth" {
