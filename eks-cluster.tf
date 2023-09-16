325b81a646c88d16ea699de68dd1833090ec99c5
Jeremy Wang <shijie.wang1990@gmail.com>
2023-09-16 17:40:23 -0500

    delete eks

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,23 +1,10 @@
+# module "eks_cluster" {
+#   source = "./modules/eks"
 
-
-# data "aws_eks_cluster_auth" "cluster" {
-#   name = "cloud-ai-demo"
-# }
-
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.cluster.endpoint
-#   token                  = data.aws_eks_cluster_auth.cluster.token
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-# }
-
-
-module "eks_cluster" {
-  source = "./modules/eks"
-
-  vpc_id          = aws_vpc.cloud-ai-vpc.id
-  subnets = [aws_subnet.cloud-ai-public-subnet-us-east-1a.id, aws_subnet.cloud-ai-public-subnet-us-east-1b.id, aws_subnet.cloud-ai-public-subnet-us-east-1c.id]
-  cluster_version = "1.27"
+#   vpc_id          = aws_vpc.cloud-ai-vpc.id
+#   subnets = [aws_subnet.cloud-ai-public-subnet-us-east-1a.id, aws_subnet.cloud-ai-public-subnet-us-east-1b.id, aws_subnet.cloud-ai-public-subnet-us-east-1c.id]
+#   cluster_version = "1.27"
   
-  cluster_name = "cloud-ai-demo"
-}
+#   cluster_name = "cloud-ai-demo"
+# }
 
