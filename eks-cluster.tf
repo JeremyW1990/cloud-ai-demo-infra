8e5a69b8406e9d0fd318ab774af6e8283182263c
Jeremy Wang <shijie.wang1990@gmail.com>
2023-09-16 15:13:01 -0500

    change all the subnets to public

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -16,7 +16,7 @@ module "eks_cluster" {
   source = "./modules/eks"
 
   vpc_id          = aws_vpc.cloud-ai-vpc.id
-  private_subnets = [aws_subnet.cloud-ai-private-subnet-us-east-1a.id, aws_subnet.cloud-ai-private-subnet-us-east-1b.id, aws_subnet.cloud-ai-private-subnet-us-east-1c.id]
+  private_subnets = [aws_subnet.cloud-ai-public-subnet-us-east-1a, aws_subnet.cloud-ai-public-subnet-us-east-1b.id, aws_subnet.cloud-ai-public-subnet-us-east-1c.id]
   cluster_version = "1.27"
   
   cluster_name = "cloud-ai-demo"
--- vpc.tf
+++ vpc.tf
@@ -1,40 +1,44 @@
 resource "aws_vpc" "cloud-ai-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "Cloud-AI VPC" 
+    Name = "Cloud-AI VPC"
   }
 }
 
-resource "aws_subnet" "cloud-ai-private-subnet-us-east-1a" {
+# Now, all three subnets are public subnets.
+resource "aws_subnet" "cloud-ai-public-subnet-us-east-1a" {
   vpc_id            = aws_vpc.cloud-ai-vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true # This attribute is crucial for a public subnet
 
   tags = {
-    Name = "Cloud-AI Private Subnet us-east-1a"
+    Name = "Cloud-AI Public Subnet us-east-1a"
   }
 }
 
-resource "aws_subnet" "cloud-ai-private-subnet-us-east-1b" {
+resource "aws_subnet" "cloud-ai-public-subnet-us-east-1b" {
   vpc_id            = aws_vpc.cloud-ai-vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "Cloud-AI Private Subnet us-east-1b"
+    Name = "Cloud-AI Public Subnet us-east-1b"
   }
 }
 
-resource "aws_subnet" "cloud-ai-private-subnet-us-east-1c" {
+resource "aws_subnet" "cloud-ai-public-subnet-us-east-1c" {
   vpc_id            = aws_vpc.cloud-ai-vpc.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "us-east-1c"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "Cloud-AI Private Subnet us-east-1c"
+    Name = "Cloud-AI Public Subnet us-east-1c"
   }
 }
 
@@ -44,7 +48,7 @@ resource "aws_security_group" "cloud-ai-allow-all" {
   vpc_id      = aws_vpc.cloud-ai-vpc.id
 
   ingress {
-    from_port   = 0 
+    from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
@@ -55,7 +59,6 @@ resource "aws_security_group" "cloud-ai-allow-all" {
   }
 }
 
-# Internet Gateway
 resource "aws_internet_gateway" "cloud-ai-igw" {
   vpc_id = aws_vpc.cloud-ai-vpc.id
 
@@ -64,59 +67,31 @@ resource "aws_internet_gateway" "cloud-ai-igw" {
   }
 }
 
-# Public subnet for NAT Gateway
-resource "aws_subnet" "cloud-ai-public-subnet-us-east-1a" {
-  vpc_id            = aws_vpc.cloud-ai-vpc.id
-  cidr_block        = "10.0.4.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "Cloud-AI Public Subnet us-east-1a"
-  }
-}
-
-# Create an Elastic IP for NAT Gateway
-resource "aws_eip" "nat_eip" {
-  vpc = true
-}
-
-# NAT Gateway
-resource "aws_nat_gateway" "cloud-ai-nat-gateway" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.cloud-ai-public-subnet-us-east-1a.id
-
-  tags = {
-    Name = "Cloud-AI NAT Gateway"
-  }
-}
-
-# Route table for private subnets to route through NAT Gateway
-resource "aws_route_table" "private-route-table" {
+# Route table for the now-public subnets
+resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.cloud-ai-vpc.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.cloud-ai-nat-gateway.id
+    gateway_id     = aws_internet_gateway.cloud-ai-igw.id
   }
 
   tags = {
-    Name = "Private Subnet Route Table"
+    Name = "Public Subnet Route Table"
   }
 }
 
-# Associate private subnets with the new route table
-resource "aws_route_table_association" "private-subnet-association-1a" {
-  subnet_id      = aws_subnet.cloud-ai-private-subnet-us-east-1a.id
-  route_table_id = aws_route_table.private-route-table.id
+resource "aws_route_table_association" "public-subnet-association-1a" {
+  subnet_id      = aws_subnet.cloud-ai-public-subnet-us-east-1a.id
+  route_table_id = aws_route_table.public-route-table.id
 }
 
-resource "aws_route_table_association" "private-subnet-association-1b" {
-  subnet_id      = aws_subnet.cloud-ai-private-subnet-us-east-1b.id
-  route_table_id = aws_route_table.private-route-table.id
+resource "aws_route_table_association" "public-subnet-association-1b" {
+  subnet_id      = aws_subnet.cloud-ai-public-subnet-us-east-1b.id
+  route_table_id = aws_route_table.public-route-table.id
 }
 
-resource "aws_route_table_association" "private-subnet-association-1c" {
-  subnet_id      = aws_subnet.cloud-ai-private-subnet-us-east-1c.id
-  route_table_id = aws_route_table.private-route-table.id
+resource "aws_route_table_association" "public-subnet-association-1c" {
+  subnet_id      = aws_subnet.cloud-ai-public-subnet-us-east-1c.id
+  route_table_id = aws_route_table.public-route-table.id
 }
-
