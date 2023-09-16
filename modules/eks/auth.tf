a4893cebc9df5777bb94dd7afb56643912636e08
Jeremy Wang <shijie.wang1990@gmail.com>
2023-09-16 16:36:32 -0500

    add auth map

--- modules/eks/auth.tf
+++ modules/eks/auth.tf
@@ -2,14 +2,14 @@ locals {
   aws_auth_config = yamldecode(file("${path.module}/aws-auth-configmap.yaml"))
 }
 
-# resource "kubernetes_config_map" "aws_auth" {
-#   metadata {
-#     name      = "aws-auth"
-#     namespace = "kube-system"
-#   }
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
 
-#   data = {
-#     mapRoles = local.aws_auth_config["mapRoles"]
-#     mapUsers = local.aws_auth_config["mapUsers"]
-#   }
-# }
+  data = {
+    mapRoles = local.aws_auth_config["mapRoles"]
+    mapUsers = local.aws_auth_config["mapUsers"]
+  }
+}
