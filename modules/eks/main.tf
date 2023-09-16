264eb2816660844c04b042b0b52b63f73c86a487
Jeremy Wang <shijie.wang1990@gmail.com>
2023-09-16 17:10:52 -0500

    remove aws_auth

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -29,7 +29,6 @@ resource "aws_eks_node_group" "this" {
   }
 
   depends_on = [ 
-    # kubernetes_config_map.aws_auth,
     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
