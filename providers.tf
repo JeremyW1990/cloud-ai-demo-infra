1b87b719a6a63f5d47e58b930efbce4d8fb3c160
Jeremy Wang <shijie.wang1990@gmail.com>
2023-09-16 17:45:15 -0500

    delete eks

--- providers.tf
+++ providers.tf
@@ -1,16 +1,16 @@
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks_cluster.cluster_name
-}
+# data "aws_eks_cluster_auth" "this" {
+#   name = module.eks_cluster.cluster_name
+# }
 
-data "aws_eks_cluster" "this" {
-  name = module.eks_cluster.cluster_name
-}
+# data "aws_eks_cluster" "this" {
+#   name = module.eks_cluster.cluster_name
+# }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.this.token
-}
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.this.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
+#   token                  = data.aws_eks_cluster_auth.this.token
+# }
 
 terraform {
   required_providers {
