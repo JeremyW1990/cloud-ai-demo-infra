afdf831e92b7523e3747ad00a117b89eb30312f6
Jeremy Wang <shijie.wang1990@gmail.com>
2023-09-16 17:55:47 -0500

    create eks again.

--- modules/eks/auth.tf
+++ modules/eks/auth.tf
@@ -2,6 +2,20 @@ locals {
   aws_auth_config = yamldecode(file("${path.module}/aws-auth-configmap.yaml"))
 }
 
+# data "aws_eks_cluster_auth" "this" {
+#   name = var.cluster_name
+# }
+
+# data "aws_eks_cluster" "this" {
+#   name = var.cluster_name
+# }
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
+  token                  = aws_eks_cluster_auth.this.token
+}
+
 resource "kubernetes_config_map" "aws_auth" {
   metadata {
     name      = "aws-auth" 
